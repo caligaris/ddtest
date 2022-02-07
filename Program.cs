044f77f86ccff1b2fe3f90f7086a8f98f92945e1
Luis Caligaris Garcia <lucali@microsoft.com>
2022-02-07 09:24:24 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using DotNetCoreSqlDb.Models;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Logging.AddAzureWebAppDiagnostics();
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<MyDatabaseContext>(options => {
+    options.UseSqlServer(builder.Configuration.GetConnectionString("MyDbConnection"));
+    //options.UseSqlite("Data Source=localdatabase.db");
+});
+
+var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+
+    try
+    {
+        var context = services.GetRequiredService<MyDatabaseContext>();
+        context.Database.EnsureCreated();
+    }
+    catch (Exception ex)
+    {
+        var logger = services.GetRequiredService<ILogger<Program>>();
+        logger.LogError(ex, "An error occurred creating the DB.");
+    }
+}
+
+//app.Services.GetRequiredService<MyDatabaseContext>().Database.Migrate();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+else {
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+// app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Todos}/{action=Index}/{id?}");
+
+app.Run();
